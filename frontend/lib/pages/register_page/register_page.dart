ad360da221b49f2ab84fc5f9c20a6b435b40d7a3
Gennady Alheev <44393437+MiiMan@users.noreply.github.com>
2022-07-14 19:37:22 +0300

    add: register_page

---
+++ frontend/lib/pages/register_page/register_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+
+}
